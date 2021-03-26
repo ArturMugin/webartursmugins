74d8c20657f78fce3388a5cb752fcbeca719a279
ArturMugin <arturjancik@gmail.com>
2021-03-26 04:46:11 +0000

    DB connection set for cPanel

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$server_name = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "1905136";
+$server_name = "www.1905136.sandwichmelody.com";
+$db_username = "sandwich_arturjancik";
+$db_password = "makaka2000";
+$db_name = "sandwich_1905136";
 
 $connection = mysqli_connect($server_name,$db_username,$db_password,$db_name);
 
